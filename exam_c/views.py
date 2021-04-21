e510f45042360aac4f901a29cf8949f7fbb02a33
tanglw <tanglw@qq.com>
2021-04-21 20:01:58 +0800

    feat: add coding question for exam page

--- exam_c/views.py
+++ exam_c/views.py
@@ -34,11 +34,19 @@ def exampage(request, exampage_id):
     for i in choice_question_ids:
         choice_questions.append(ChoiceQuestion.objects.get(pk=i))
 
+    coding_question_ids = [int(x) for x in exam_page.coding_questions.split(',') if len(x)]
+    coding_questions = []
+    for i in coding_question_ids:
+        coding_questions.append(CodingQuestion.objects.get(pk=i))
+
     context = {
         'exam': exam,
         'student': student,
         'exam_page': exam_page,
-        'choice_questions': choice_questions
+        'choice_questions': choice_questions,
+        'coding_questions': coding_questions,
+        'choice_questions_answers': exam_page.choice_question_answers.split(','),
+        'coding_questions_answers': exam_page.coding_question_answers.split(','),
         }
     return render(request, 'exam_c/exam_page.html', context)
 
@@ -78,18 +86,25 @@ def login(request):
             exam = Exam.objects.get(pk=exam_id)
             student = Student.objects.get(student_id=student_id)
 
-            # 检查该考场和学号对应的试卷是否存在，不存在就创建新的
+            # 检查该考场和学号对应的试卷是否存在，不存在就创建新的试卷
             # 同时随机抽取题目
-            choice_question_numb, coding_question_numb = 10, 4
+            choice_question_numb, coding_question_numb = 10, 2
             if not ExamPaper.objects.filter(student=student, exam=exam).exists():
                 exam_paper = ExamPaper.objects.create(student=student, exam=exam)
                 exam_paper.problem_type = exam.problem_type
                 exam_paper.start_time = datetime.datetime.now()
 
+                #########
                 choice_questions_ids = [str(x['id']) for x in ChoiceQuestion.objects.values('id')]
                 choice_questions_ids = random.sample(choice_questions_ids, choice_question_numb)
                 exam_paper.choice_questions = ','.join(choice_questions_ids)
                 exam_paper.choice_question_answers = ','.join(['+' for i in range(len(choice_questions_ids))])
+
+                #########
+                code_questions_ids = [str(x['id']) for x in CodingQuestion.objects.values('id')]
+                code_questions_ids = random.sample(code_questions_ids, coding_question_numb)
+                exam_paper.coding_questions = ','.join(code_questions_ids)
+                exam_paper.coding_question_answers = ','.join(['+' for i in range(len(code_questions_ids))])
                 exam_paper.save()
             else:
                 exam_paper = ExamPaper.objects.get(student=student, exam=exam)
